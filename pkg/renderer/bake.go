1fcea5820e43e45c622fa889448597621cb8d71a
simpala <simpala@users.noreply.github.com>
2026-01-26 23:27:37 -0800

    Update bake.go

--- pkg/renderer/bake.go
+++ pkg/renderer/bake.go
@@ -32,18 +32,18 @@ type BakedAtom struct {
 // TLASNode represents a node in the Top-Level Acceleration Structure.
 type TLASNode struct {
 	Min, Max    [3]float32
-	BLASOffset  int64  // Absolute file offset to the root BLASNode of a shape
-	IsLeaf      int32  // 1 = Leaf (points to BLAS), 0 = Node
-	Left, Right int32  // Relative indices to other TLASNodes
+	BLASOffset  int64 // Absolute file offset to the root BLASNode of a shape
+	IsLeaf      int32 // 1 = Leaf (points to BLAS), 0 = Node
+	Left, Right int32 // Relative indices to other TLASNodes
 	Padding     int32
 }
 
 // BLASNode represents a node in the Bottom-Level Acceleration Structure.
 type BLASNode struct {
 	Min, Max    [3]float32
-	AtomOffset  int64  // Absolute file offset to the first atom in a leaf
-	AtomCount   int32  // Number of atoms in leaf, 0 for internal nodes
-	Left, Right int32  // Relative indices to other BLASNodes in the same shape's block
+	AtomOffset  int64 // Absolute file offset to the first atom in a leaf
+	AtomCount   int32 // Number of atoms in leaf, 0 for internal nodes
+	Left, Right int32 // Relative indices to other BLASNodes in the same shape's block
 	Padding     int32
 }
 
@@ -77,7 +77,9 @@ type blasResult struct {
 
 func OctEncode(n math.Point3D) uint32 {
 	l1 := gomath.Abs(n.X) + gomath.Abs(n.Y) + gomath.Abs(n.Z)
-	if l1 == 0 { return 0 }
+	if l1 == 0 {
+		return 0
+	}
 	x, y := n.X/l1, n.Y/l1
 	if n.Z < 0 {
 		oldX := x
@@ -104,12 +106,14 @@ func OctDecode(packed uint32) math.Point3D {
 }
 
 func sign(f float64) float64 {
-	if f >= 0 { return 1 }
+	if f >= 0 {
+		return 1
+	}
 	return -1
 }
 
 func (a *BakedAtom) Write(w io.Writer) error { return binary.Write(w, binary.LittleEndian, a) }
-func (a *BakedAtom) Read(r io.Reader) error { return binary.Read(r, binary.LittleEndian, a) }
+func (a *BakedAtom) Read(r io.Reader) error  { return binary.Read(r, binary.LittleEndian, a) }
 
 type BakeEngine struct {
 	Camera   camera.Camera
@@ -130,7 +134,9 @@ type BakeEngine struct {
 
 func NewBakeEngine(cam camera.Camera, shapes []geometry.Shape, light shading.Light, width, height int, minSize, near, far, shutter float64, target, up math.Point3D, fov float64) *BakeEngine {
 	shapeIDs := make(map[geometry.Shape]uint8)
-	for i, s := range shapes { shapeIDs[s] = uint8(i) }
+	for i, s := range shapes {
+		shapeIDs[s] = uint8(i)
+	}
 	return &BakeEngine{
 		Camera: cam, Shapes: shapes, Light: light, Width: width, Height: height,
 		MinSize: minSize, Near: near, Far: far, Shutter: shutter,
@@ -141,7 +147,9 @@ func NewBakeEngine(cam camera.Camera, shapes []geometry.Shape, light shading.Lig
 func (e *BakeEngine) Bake(tempFile string, finalFile string) error {
 	fmt.Printf("Starting Pass A (The Raw Bake)... writing to %s\n", tempFile)
 	f, err := os.Create(tempFile)
-	if err != nil { return err }
+	if err != nil {
+		return err
+	}
 	defer f.Close()
 	initialAABB := math.AABB3D{Min: math.Point3D{X: 0, Y: 0, Z: e.Near}, Max: math.Point3D{X: 1, Y: 1, Z: e.Far}}
 	bvh := geometry.NewBVH(e.Shapes)
@@ -154,19 +162,25 @@ func (e *BakeEngine) Bake(tempFile string, finalFile string) error {
 func (e *BakeEngine) Indexer(tempFile string, finalFile string, totalAtoms int64) error {
 	fmt.Printf("Starting Pass B (The Indexer)... writing to %s\n", finalFile)
 	f, err := os.Open(tempFile)
-	if err != nil { return err }
+	if err != nil {
+		return err
+	}
 	defer f.Close()
 	atomsByShape := make(map[uint8][]BakedAtom)
 	for {
 		var atom BakedAtom
 		if err := atom.Read(f); err != nil {
-			if err == io.EOF { break }
+			if err == io.EOF {
+				break
+			}
 			return err
 		}
 		atomsByShape[atom.MaterialID] = append(atomsByShape[atom.MaterialID], atom)
 	}
 	out, err := os.Create(finalFile)
-	if err != nil { return err }
+	if err != nil {
+		return err
+	}
 	defer out.Close()
 	header := Header{
 		Magic: [4]byte{'S', 'D', 'S', 'B'}, Version: 1, AtomCount: totalAtoms,
@@ -175,10 +189,10 @@ func (e *BakeEngine) Indexer(tempFile string, finalFile string, totalAtoms int64
 	}
 	eye := e.Camera.GetEye()
 	header.BakeCamera = CameraData{
-		Eye: [3]float32{float32(eye.X), float32(eye.Y), float32(eye.Z)},
+		Eye:    [3]float32{float32(eye.X), float32(eye.Y), float32(eye.Z)},
 		Target: [3]float32{float32(e.CamTarget.X), float32(e.CamTarget.Y), float32(e.CamTarget.Z)},
-		Up: [3]float32{float32(e.CamUp.X), float32(e.CamUp.Y), float32(e.CamUp.Z)},
-		Fov: float32(e.CamFov), Aspect: float32(float64(e.Width) / float64(e.Height)),
+		Up:     [3]float32{float32(e.CamUp.X), float32(e.CamUp.Y), float32(e.CamUp.Z)},
+		Fov:    float32(e.CamFov), Aspect: float32(float64(e.Width) / float64(e.Height)),
 		Near: float32(e.Near), Far: float32(e.Far),
 	}
 	binary.Write(out, binary.LittleEndian, header)
@@ -188,14 +202,18 @@ func (e *BakeEngine) Indexer(tempFile string, finalFile string, totalAtoms int64
 		fmt.Printf("Building BLAS for Shape %d (%d atoms)...\n", shapeID, len(atoms))
 		nodes, sortedAtoms := e.buildBLAS(atoms)
 		atomStartOffset, _ := out.Seek(0, io.SeekCurrent)
-		for _, a := range sortedAtoms { a.Write(out) }
+		for _, a := range sortedAtoms {
+			a.Write(out)
+		}
 		blasStartOffset, _ := out.Seek(0, io.SeekCurrent)
 		for i := range nodes {
 			if nodes[i].AtomCount > 0 {
 				nodes[i].AtomOffset = atomStartOffset + int64(nodes[i].AtomOffset)
 			}
 		}
-		for _, n := range nodes { binary.Write(out, binary.LittleEndian, n) }
+		for _, n := range nodes {
+			binary.Write(out, binary.LittleEndian, n)
+		}
 		shapeAABB := math.AABB3D{
 			Min: math.Point3D{X: float64(nodes[0].Min[0]), Y: float64(nodes[0].Min[1]), Z: float64(nodes[0].Min[2])},
 			Max: math.Point3D{X: float64(nodes[0].Max[0]), Y: float64(nodes[0].Max[1]), Z: float64(nodes[0].Max[2])},
@@ -204,7 +222,9 @@ func (e *BakeEngine) Indexer(tempFile string, finalFile string, totalAtoms int64
 	}
 	tlasNodes := e.buildTLAS(blasResults)
 	tlasStartOffset, _ := out.Seek(0, io.SeekCurrent)
-	for _, n := range tlasNodes { binary.Write(out, binary.LittleEndian, n) }
+	for _, n := range tlasNodes {
+		binary.Write(out, binary.LittleEndian, n)
+	}
 	header.TLASRoot = tlasStartOffset
 	out.Seek(0, io.SeekStart)
 	binary.Write(out, binary.LittleEndian, header)
@@ -213,28 +233,45 @@ func (e *BakeEngine) Indexer(tempFile string, finalFile string, totalAtoms int64
 }
 
 func (e *BakeEngine) buildBLAS(atoms []BakedAtom) ([]BLASNode, []BakedAtom) {
-	if len(atoms) == 0 { return nil, nil }
+	if len(atoms) == 0 {
+		return nil, nil
+	}
 	minP, maxP := atoms[0].Pos, atoms[0].Pos
 	for _, a := range atoms {
 		for i := 0; i < 3; i++ {
-			if a.Pos[i] < minP[i] { minP[i] = a.Pos[i] }
-			if a.Pos[i] > maxP[i] { maxP[i] = a.Pos[i] }
+			if a.Pos[i] < minP[i] {
+				minP[i] = a.Pos[i]
+			}
+			if a.Pos[i] > maxP[i] {
+				maxP[i] = a.Pos[i]
+			}
 		}
 	}
 	sceneAABB := math.AABB3D{Min: math.Point3D{X: float64(minP[0]), Y: float64(minP[1]), Z: float64(minP[2])}, Max: math.Point3D{X: float64(maxP[0]), Y: float64(maxP[1]), Z: float64(maxP[2])}}
 	diag := sceneAABB.Max.Sub(sceneAABB.Min)
-	type atomWithCode struct { atom BakedAtom; code uint32 }
+	type atomWithCode struct {
+		atom BakedAtom
+		code uint32
+	}
 	codedAtoms := make([]atomWithCode, len(atoms))
 	for i, a := range atoms {
 		nx, ny, nz := 0.5, 0.5, 0.5
-		if diag.X > 0 { nx = (float64(a.Pos[0]) - sceneAABB.Min.X) / diag.X }
-		if diag.Y > 0 { ny = (float64(a.Pos[1]) - sceneAABB.Min.Y) / diag.Y }
-		if diag.Z > 0 { nz = (float64(a.Pos[2]) - sceneAABB.Min.Z) / diag.Z }
+		if diag.X > 0 {
+			nx = (float64(a.Pos[0]) - sceneAABB.Min.X) / diag.X
+		}
+		if diag.Y > 0 {
+			ny = (float64(a.Pos[1]) - sceneAABB.Min.Y) / diag.Y
+		}
+		if diag.Z > 0 {
+			nz = (float64(a.Pos[2]) - sceneAABB.Min.Z) / diag.Z
+		}
 		codedAtoms[i] = atomWithCode{atom: a, code: math.Morton3D(nx, ny, nz)}
 	}
 	sort.Slice(codedAtoms, func(i, j int) bool { return codedAtoms[i].code < codedAtoms[j].code })
 	sortedAtoms := make([]BakedAtom, len(atoms))
-	for i, ca := range codedAtoms { sortedAtoms[i] = ca.atom }
+	for i, ca := range codedAtoms {
+		sortedAtoms[i] = ca.atom
+	}
 	var nodes []BLASNode
 	var build func(start, end int) int32
 	build = func(start, end int) int32 {
@@ -243,8 +280,12 @@ func (e *BakeEngine) buildBLAS(atoms []BakedAtom) ([]BLASNode, []BakedAtom) {
 		curMin, curMax := sortedAtoms[start].Pos, sortedAtoms[start].Pos
 		for i := start + 1; i < end; i++ {
 			for j := 0; j < 3; j++ {
-				if sortedAtoms[i].Pos[j] < curMin[j] { curMin[j] = sortedAtoms[i].Pos[j] }
-				if sortedAtoms[i].Pos[j] > curMax[j] { curMax[j] = sortedAtoms[i].Pos[j] }
+				if sortedAtoms[i].Pos[j] < curMin[j] {
+					curMin[j] = sortedAtoms[i].Pos[j]
+				}
+				if sortedAtoms[i].Pos[j] > curMax[j] {
+					curMax[j] = sortedAtoms[i].Pos[j]
+				}
 			}
 		}
 		nodes[nodeIdx].Min, nodes[nodeIdx].Max = curMin, curMax
@@ -264,14 +305,18 @@ func (e *BakeEngine) buildBLAS(atoms []BakedAtom) ([]BLASNode, []BakedAtom) {
 }
 
 func (e *BakeEngine) buildTLAS(blasInfos []blasResult) []TLASNode {
-	if len(blasInfos) == 0 { return nil }
+	if len(blasInfos) == 0 {
+		return nil
+	}
 	var nodes []TLASNode
 	var build func(infos []blasResult) int32
 	build = func(infos []blasResult) int32 {
 		nodeIdx := int32(len(nodes))
 		nodes = append(nodes, TLASNode{Left: -1, Right: -1})
 		overallAABB := infos[0].aabb
-		for i := 1; i < len(infos); i++ { overallAABB = overallAABB.Expand(infos[i].aabb.Min).Expand(infos[i].aabb.Max) }
+		for i := 1; i < len(infos); i++ {
+			overallAABB = overallAABB.Expand(infos[i].aabb.Min).Expand(infos[i].aabb.Max)
+		}
 		nodes[nodeIdx].Min = [3]float32{float32(overallAABB.Min.X), float32(overallAABB.Min.Y), float32(overallAABB.Min.Z)}
 		nodes[nodeIdx].Max = [3]float32{float32(overallAABB.Max.X), float32(overallAABB.Max.Y), float32(overallAABB.Max.Z)}
 		if len(infos) == 1 {
@@ -294,7 +339,12 @@ func (e *BakeEngine) computeAABBWorld(aabb math.AABB3D) math.AABB3D {
 	var res math.AABB3D
 	for _, c := range corners {
 		p := e.Camera.Project(c.X, c.Y, c.Z)
-		if first { res = math.AABB3D{Min: p, Max: p}; first = false } else { res = res.Expand(p) }
+		if first {
+			res = math.AABB3D{Min: p, Max: p}
+			first = false
+		} else {
+			res = res.Expand(p)
+		}
 	}
 	return res
 }
@@ -302,7 +352,9 @@ func (e *BakeEngine) computeAABBWorld(aabb math.AABB3D) math.AABB3D {
 func (e *BakeEngine) subdivideBake(aabb math.AABB3D, w io.Writer, bvh *geometry.BVH, atomCount *int64) {
 	worldAABB := e.computeAABBWorld(aabb)
 	shapes := bvh.IntersectsShapes(worldAABB)
-	if len(shapes) == 0 { return }
+	if len(shapes) == 0 {
+		return
+	}
 	if (aabb.Max.X - aabb.Min.X) < e.MinSize {
 		// Surface Pruning: discard if entirely inside a single solid shape.
 		// !IsVolumetric() identifies solid geometry (vs participating media),
@@ -326,23 +378,27 @@ func (e *BakeEngine) subdivideBake(aabb math.AABB3D, w io.Writer, bvh *geometry.
 		for _, s := range shapes {
 			if s.Contains(worldP, 0) {
 				id, ok := e.shapeIDs[s]
-				if !ok { continue }
+				if !ok {
+					continue
+				}
 				albedo, normal := s.GetColor(), s.NormalAtPoint(worldP, 0)
 				lightDir := e.Light.Position.Sub(worldP).Normalize()
-				checkP := worldP.Add(normal.ToVector().Mul(1e-4))
-				attenuation := shading.CalculateShadowAttenuation(checkP, e.Light.Position, e.Shapes, e.Light.Radius, 0)
-				lIntensity := e.Light.Intensity * attenuation
+				//checkP := worldP.Add(normal.ToVector().Mul(1e-4))
+				//attenuation := shading.CalculateShadowAttenuation(checkP, e.Light.Position, e.Shapes, e.Light.Radius, 0)
+				//lIntensity := e.Light.Intensity * attenuation
+				lIntensity := e.Light.Intensity // we dont ever want to bake approximated shadows.
 				pCorner := e.Camera.Project(aabb.Max.X, aabb.Max.Y, aabb.Max.Z)
 				halfExtent := pCorner.Sub(worldP).Length()
 				atom := BakedAtom{
-					Pos: [3]float32{float32(worldP.X), float32(worldP.Y), float32(worldP.Z)},
+					Pos:        [3]float32{float32(worldP.X), float32(worldP.Y), float32(worldP.Z)},
 					HalfExtent: float32(halfExtent),
-					Normal: OctEncode(normal.ToVector()),
-					Albedo: [3]uint8{albedo.R, albedo.G, albedo.B}, MaterialID: id,
-					LightDir: OctEncode(lightDir),
+					Normal:     OctEncode(normal.ToVector()),
+					Albedo:     [3]uint8{albedo.R, albedo.G, albedo.B}, MaterialID: id,
+					LightDir:   OctEncode(lightDir),
 					LightColor: [3]uint8{uint8(gomath.Min(255, 255*lIntensity)), uint8(gomath.Min(255, 255*lIntensity)), uint8(gomath.Min(255, 255*lIntensity))},
 				}
-				atom.Write(w); *atomCount++
+				atom.Write(w)
+				*atomCount++
 			}
 		}
 		return
@@ -360,7 +416,9 @@ func (e *BakeEngine) subdivideBake(aabb math.AABB3D, w io.Writer, bvh *geometry.
 
 func (e *BakeEngine) Verify(bakedFile string) error {
 	scene, err := LoadBakedScene(bakedFile)
-	if err != nil { return err }
+	if err != nil {
+		return err
+	}
 	defer scene.Close()
 	fmt.Printf("Verifying baked scene %s...\nAtoms: %d, TLASRoot offset: %d\n", bakedFile, scene.Header.AtomCount, scene.Header.TLASRoot)
 	for y := 0.4; y <= 0.6; y += 0.05 {
@@ -368,7 +426,11 @@ func (e *BakeEngine) Verify(bakedFile string) error {
 			pNear, pFar := e.Camera.Project(x, y, e.Near), e.Camera.Project(x, y, e.Far)
 			ray := math.Ray{Origin: pNear, Direction: pFar.Sub(pNear).Normalize()}
 			hit, atom := scene.Intersect(ray)
-			if hit { fmt.Printf("Ray at (%.2f, %.2f): HIT shape %d at (%.2f, %.2f, %.2f)\n", x, y, atom.MaterialID, atom.Pos[0], atom.Pos[1], atom.Pos[2]) } else { fmt.Printf("Ray at (%.2f, %.2f): MISS\n", x, y) }
+			if hit {
+				fmt.Printf("Ray at (%.2f, %.2f): HIT shape %d at (%.2f, %.2f, %.2f)\n", x, y, atom.MaterialID, atom.Pos[0], atom.Pos[1], atom.Pos[2])
+			} else {
+				fmt.Printf("Ray at (%.2f, %.2f): MISS\n", x, y)
+			}
 		}
 	}
 	return nil
@@ -441,7 +503,9 @@ func LoadBakedScene(filename string, memLimit ...int64) (*BakedScene, error) {
 	return &BakedScene{Header: header, Data: data, closer: closer}, nil
 }
 
-func (s *BakedScene) Intersect(ray math.Ray) (bool, BakedAtom) { return s.intersectTLAS(s.Header.TLASRoot, ray) }
+func (s *BakedScene) Intersect(ray math.Ray) (bool, BakedAtom) {
+	return s.intersectTLAS(s.Header.TLASRoot, ray)
+}
 
 func (s *BakedScene) getTLASNode(offset int64) TLASNode {
 	data := s.Data[offset:]
